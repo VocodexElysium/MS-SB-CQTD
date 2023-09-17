8c1dbc8b508b971a3dec4794b077f9d756b2a345
VocodexElysium <vocodexelysium@gmail.com>
2023-09-17 22:46:22 +0800

    2023/9/17 zoom-in effect

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ def move_file(folders, datasets, uids, result):
                 old_file = folder + "/{}".format(dataset) + "/{}.wav".format(uid)
             else:
                 old_file = folder + "/{}".format(dataset) + "/{}_pred.wav".format(uid)
-            new_file = result + "/seen_singer_{}_{}.wav".format(i + 1, setting)
+            new_file = result + "/unseen_singer_{}_{}.wav".format(i + 1, setting)
             print(old_file)
             print(new_file)
             shutil.copyfile(old_file, new_file)
@@ -49,4 +49,28 @@ if __name__ == "__main__":
     #     "D:/Study/CUHKSZ/Research_Group/ICASSP2024/tmp/hifigan_merge",
     # ]
     # result = "D:/Study/CUHKSZ/Research_Group/ICASSP2024/MS-SB-CQTD/assets/audios/effectiveness"
-    print(get_pinyin("谁遇见多愁善感的你"))
+    ### Unseen
+    # datasets = [
+    #     "popcs",
+    #     "popcs",
+    #     "opensinger",
+    #     "opensinger",
+    #     "opensinger",
+    # ]
+    # uids = [
+    #     "明天会更好_0016",
+    #     "隐形的翅膀_0009",
+    #     "Man_21_丑八怪_8",
+    #     "Man_0_大鱼_19",
+    #     "Woman_40_易燃易爆炸_12",
+    # ]
+    # folders = [
+    #     "D:/Study/CUHKSZ/Research_Group/ICASSP2024/tmp/gt",
+    #     "D:/Study/CUHKSZ/Research_Group/ICASSP2024/tmp/hifigan_orig",
+    #     "D:/Study/CUHKSZ/Research_Group/ICASSP2024/tmp/hifigan_msstftd",
+    #     "D:/Study/CUHKSZ/Research_Group/ICASSP2024/tmp/hifigan_mscqtd",
+    #     "D:/Study/CUHKSZ/Research_Group/ICASSP2024/tmp/hifigan_merge",
+    # ]
+    # result = "D:/Study/CUHKSZ/Research_Group/ICASSP2024/MS-SB-CQTD/assets/audios/effectiveness"
+    # move_file(folders, datasets, uids, result)
+    print(get_pinyin("为我撩人,还为我双眸失神"))
